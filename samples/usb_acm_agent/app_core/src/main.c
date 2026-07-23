836bfecf3adc2ecc06866d0cb5513de14d58170b
tangzhenye <zytang@linkedsemi.com>
2026-07-23 19:09:07 +0800

    usb_acm_agent: add shell instances with switching logic decoupled from uart_agent

--- samples/usb_acm_agent/app_core/src/main.c
+++ samples/usb_acm_agent/app_core/src/main.c
@@ -1,9 +1,13 @@
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 int main(void)
 {
-	while (true) {
-		k_msleep(1000);
-	}
-	return 0;
+    while (true) {
+        LOG_INF("app core hello world!!");
+        k_msleep(1000);
+    }
+    return 0;
 }
